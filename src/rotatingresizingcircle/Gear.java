e57046edb4b4211580d9060cf185c6745dc39cbf
Keith Emery <kmne68@gmail.com>
2016-07-14 14:03:23 -0500

    Started adding gear variables.

--- src/rotatingresizingcircle/Gear.java
+++ src/rotatingresizingcircle/Gear.java
@@ -7,7 +7,20 @@ package rotatingresizingcircle;
 /**
  *
  * @author Keith
+ * 
+ * For gears to mesh, their modules must be equal.
+ * 
  */
 public class Gear {
+    
+    int period;                         // time to complete one rotation
+    int teeth;                          // number of teeth on the gear
+    double rotationalFrequency;         // rotations per unit time
+    double module;                      // a rational number, indicates the tooth size and is the number of mm of pitch circle diameter (p.c.d.) per tooth
+    boolean isWheel;                    // the larger of two interacting gears
+    boolean isPinion;                   // the smaller of two interacting gears
+    
+    
+    
     
 }
