bd1a131a595e6ab5f64c62bd504b1bd41b25432f
kmne68 <kmne68@gmail.com>
2016-07-15 00:07:58 -0500

    Continued adding gear variables.

--- src/rotatingresizingcircle/Gear.java
+++ src/rotatingresizingcircle/Gear.java
@@ -4,6 +4,8 @@
  */
 package rotatingresizingcircle;
 
+import java.awt.Point;
+
 /**
  *
  * @author Keith
@@ -19,7 +21,15 @@ public class Gear {
     double module;                      // a rational number, indicates the tooth size and is the number of mm of pitch circle diameter (p.c.d.) per tooth
     boolean isWheel;                    // the larger of two interacting gears
     boolean isPinion;                   // the smaller of two interacting gears
-    
+    double pitchDiameter;               // Value is based on the number of teeth, the normal module (or normal diametral pitch), and the helix angle.
+    Point axis;                         // Center of gear.
+    double angleOfAction;               // Angle with vertex at the gear center, one leg on the point where mating teeth first make contact, the other leg on the point where they disengage.
+    double outsideDiameter;             // Diameter at tips of teeth
+    double rootDiameter;                // Diameter at the base of the teet
+    double addendum;                    // Radial distance from the pitch surface to the outermost point of the tooth. {\displaystyle a=(D_{o}-D)/2} a=(D_o-D)/2
+    double dedendum;                    // Radial distance from the depth of the tooth trough to the pitch surface. {\displaystyle b=(D-{\text{root diameter}})/2} b=(D-\text{root diameter})/2
+    double wholeDepth;                  // Addendum plus dedendum   
+    double diametralPitch;              // Ratio of the number of teeth to the pitch diameter
     
     
     
